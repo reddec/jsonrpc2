e0eb7cd42b4851ed2cc1feb1ca32346a9e8b9bad
Alexander Baryshnikov <dev@baryshnikov.net>
2020-01-14 18:38:13 +0800

    support multy-name args

--- cmd/jsonrpc2/internal/generator.go
+++ cmd/jsonrpc2/internal/generator.go
@@ -45,37 +45,40 @@ func (wg *WrapperGenerator) generateFunction(info *Interface, fs *token.FileSet,
 
 func (wg *WrapperGenerator) generateLambda(method *Method, fs *token.FileSet, file *ast.File) jen.Code {
 	return jen.Func().Params(jen.Id("params").Qual("encoding/json", "RawMessage"), jen.Id("positional").Bool()).Call(jen.Interface(), jen.Error()).BlockFunc(func(group *jen.Group) {
+		var argNames []string
 		group.Var().Id("args").StructFunc(func(st *jen.Group) {
 			if method.Type.Params == nil {
 				return
 			}
-			var argNames = make([]string, len(method.Type.Params.List))
-			for i, arg := range method.Type.Params.List {
-				name := "Arg" + strconv.Itoa(i)
-				argNames[i] = name
-				st.Id(name).Id(astPrint(arg.Type, fs)).Tag(map[string]string{
-					"json": arg.Names[0].Name,
-				})
+			argNames = make([]string, 0, len(method.Type.Params.List))
+			for _, arg := range method.Type.Params.List {
+				for _, argName := range arg.Names {
+					name := "Arg" + strconv.Itoa(len(argNames))
+					argNames = append(argNames, name)
+					st.Id(name).Id(astPrint(arg.Type, fs)).Tag(map[string]string{
+						"json": argName.Name,
+					})
+				}
 			}
-			group.Var().Id("err").Error()
-			group.If().Id("positional").BlockFunc(func(pos *jen.Group) {
-				pos.Err().Op("=").Qual(Import, "UnmarshalArray").CallFunc(func(params *jen.Group) {
-					params.Id("params")
-					for _, arg := range argNames {
-						params.Op("&").Id("args").Dot(arg)
-					}
-				})
-			}).Else().BlockFunc(func(named *jen.Group) {
-				named.Err().Op("=").Qual("encoding/json", "Unmarshal").Call(jen.Id("params"), jen.Op("&").Id("args"))
-			})
-			group.If().Err().Op("!=").Nil().BlockFunc(func(failed *jen.Group) {
-				failed.Return(jen.Nil(), jen.Err())
-			})
-			group.Return().Id("wrap").Dot(method.Name).CallFunc(func(params *jen.Group) {
+		})
+		group.Var().Id("err").Error()
+		group.If().Id("positional").BlockFunc(func(pos *jen.Group) {
+			pos.Err().Op("=").Qual(Import, "UnmarshalArray").CallFunc(func(params *jen.Group) {
+				params.Id("params")
 				for _, arg := range argNames {
-					params.Id("args").Dot(arg)
+					params.Op("&").Id("args").Dot(arg)
 				}
 			})
+		}).Else().BlockFunc(func(named *jen.Group) {
+			named.Err().Op("=").Qual("encoding/json", "Unmarshal").Call(jen.Id("params"), jen.Op("&").Id("args"))
+		})
+		group.If().Err().Op("!=").Nil().BlockFunc(func(failed *jen.Group) {
+			failed.Return(jen.Nil(), jen.Err())
+		})
+		group.Return().Id("wrap").Dot(method.Name).CallFunc(func(params *jen.Group) {
+			for _, arg := range argNames {
+				params.Id("args").Dot(arg)
+			}
 		})
 	})
 }
