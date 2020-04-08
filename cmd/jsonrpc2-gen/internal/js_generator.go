4cef8a0eda1a029cafd45e587c23005cb4b1c077
Alexander Baryshnikov <dev@baryshnikov.net>
2020-04-08 18:23:44 +0800

    beutify js methods

--- cmd/jsonrpc2-gen/internal/js_generator.go
+++ cmd/jsonrpc2-gen/internal/js_generator.go
@@ -2,11 +2,17 @@ package internal
 
 import (
 	"bytes"
+	"github.com/Masterminds/sprig"
+	"strings"
 	"text/template"
 )
 
 func (result *generationResult) GenerateJS() string {
-	t := template.Must(template.New("").Parse(string(MustAsset("js.gotemplate"))))
+	fm := sprig.TxtFuncMap()
+	fm["firstLine"] = func(text string) string {
+		return strings.Split(text, "\n")[0]
+	}
+	t := template.Must(template.New("").Funcs(fm).Parse(string(MustAsset("js.gotemplate"))))
 	buffer := &bytes.Buffer{}
 	err := t.Execute(buffer, result)
 	if err != nil {
