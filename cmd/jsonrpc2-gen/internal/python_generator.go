184dc4112c1457c3cc10f63a923abd684bedf1e0
Alexander Baryshnikov <dev@baryshnikov.net>
2020-04-13 19:58:15 +0800

    add typing to python

--- cmd/jsonrpc2-gen/internal/python_generator.go
+++ cmd/jsonrpc2-gen/internal/python_generator.go
@@ -11,7 +11,7 @@ import (
 
 func (result *generationResult) GeneratePython() string {
 	var python Python
-
+	python.BeforeInspect = deepparser.RemoveJsonIgnoredFields
 	fm := sprig.TxtFuncMap()
 	fm["python"] = func(field interface{}) string {
 		switch v := field.(type) {
@@ -28,6 +28,58 @@ func (result *generationResult) GeneratePython() string {
 	fm["firstLine"] = func(text string) string {
 		return strings.Split(text, "\n")[0]
 	}
+	fm["isdefined"] = func(st *deepparser.StField) bool {
+		return python.isDefined(deepparser.RebuildTypeNameWithoutPackage(st.AST.Type))
+	}
+	fm["to_json"] = func(field interface{}, param string) string {
+		var st ast.Expr
+		switch v := field.(type) {
+		case typed:
+			st = v.AST
+		case arg:
+			st = v.AST
+		case *deepparser.StField:
+			st = v.AST.Type
+		default:
+			panic("ts?")
+		}
+
+		if python.isBytes(st) {
+			return "encodebytes(" + param + ")"
+		}
+		if !python.isDefined(deepparser.RebuildTypeNameWithoutPackage(st)) {
+			return param
+		}
+		if isArray(st) {
+			return "[x.to_json() for x in " + param + "]"
+		}
+		return param + ".to_json()"
+	}
+	fm["from_json"] = func(field interface{}, param string) string {
+		var st ast.Expr
+		switch v := field.(type) {
+		case typed:
+			st = v.AST
+		case arg:
+			st = v.AST
+		case *deepparser.StField:
+			st = v.AST.Type
+		default:
+			panic("ts?")
+		}
+
+		if python.isBytes(st) {
+			return "decodebytes(" + param + ")"
+		}
+		subType := deepparser.RebuildTypeNameWithoutPackage(st)
+		if !python.isDefined(subType) {
+			return param
+		}
+		if isArray(st) {
+			return "[" + subType + ".from_json(x) for x in " + param + "]"
+		}
+		return subType + ".from_json(" + param + ")"
+	}
 	fm["definitions"] = func() []*deepparser.Definition {
 		if python.Ordered == nil {
 			for _, mth := range result.UsedMethods {
@@ -36,7 +88,13 @@ func (result *generationResult) GeneratePython() string {
 				}
 			}
 		}
-		return python.Ordered
+		var ans []*deepparser.Definition
+		for _, d := range python.Ordered {
+			if len(d.StructFields()) > 0 {
+				ans = append(ans, d)
+			}
+		}
+		return ans
 	}
 	fm["imports"] = func() map[string]map[string]bool {
 		return python.Modules
@@ -47,7 +105,19 @@ func (result *generationResult) GeneratePython() string {
 	if err != nil {
 		panic(err)
 	}
-	return buffer.String()
+	txt := buffer.String()
+
+	var imps []string
+	for imp, mods := range python.Modules {
+		var names []string
+		for name := range mods {
+			names = append(names, name)
+		}
+		if len(names) > 0 {
+			imps = append(imps, "from "+imp+" import "+strings.Join(names, ", "))
+		}
+	}
+	return strings.Replace(txt, "# imports", strings.Join(imps, "\n"), 1)
 }
 
 type Python struct {
@@ -77,11 +147,21 @@ func (py *Python) mapBase(typeName string) string {
 }
 
 func (py *Python) MapType(t ast.Expr) string {
+	if py.isBytes(t) {
+		py.AddModule("base64", "encodebytes", "decodebytes")
+		return "bytes"
+	}
 	if v, ok := t.(*ast.Ident); ok {
+		if py.isDefined(v.Name) {
+			return v.Name
+		}
 		return py.mapBase(v.Name)
 	}
-	if _, ok := t.(*ast.SelectorExpr); ok {
+	if acc, ok := t.(*ast.SelectorExpr); ok {
 		py.AddModule("typing", "Any")
+		if py.isDefined(acc.Sel.Name) {
+			return acc.Sel.Name
+		}
 		return "Any"
 	}
 	if ptr, ok := t.(*ast.StarExpr); ok {
@@ -89,8 +169,8 @@ func (py *Python) MapType(t ast.Expr) string {
 	}
 
 	if arr, ok := t.(*ast.ArrayType); ok {
-		py.AddModule("typing", "List")
-		return "List[" + py.MapType(arr.Elt) + "]"
+		py.AddModule("typing", "List", "Optional")
+		return "Optional[List[" + py.MapType(arr.Elt) + "]]"
 	}
 	py.AddModule("typing", "Any")
 	return "Any"
@@ -118,4 +198,27 @@ func (py *Python) AddModule(pyImportName string, names ...string) {
 	for _, name := range names {
 		py.Modules[pyImportName][name] = true
 	}
+}
+
+func (py *Python) isDefined(name string) bool {
+	for _, p := range py.Ordered {
+		if p.TypeName == name && len(p.StructFields()) > 0 {
+			return true
+		}
+	}
+	return false
+}
+
+func (py *Python) isBytes(tp ast.Expr) bool {
+	if s, ok := tp.(*ast.ArrayType); ok {
+		if e, ok := s.Elt.(*ast.Ident); ok {
+			return e.Name == "byte"
+		}
+	}
+	return false
+}
+
+func isArray(tp ast.Expr) bool {
+	_, ok := tp.(*ast.ArrayType)
+	return ok
 }
